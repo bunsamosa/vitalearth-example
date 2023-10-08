ba29daeb2bcc6bebabe7267fd2c21f3b20963df0
madhukar01 <mholla8@gmail.com>
2023-10-07 23:46:32 -0400

    feat(init): initial example app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {authClient} from "hulk-sdk";
+
+const client = new authClient("main", "TetsApp");
+
+async function login() {
+	const address = await client.login();
+	console.log(address);
+}
+</script>
+
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to Skeleton.</h2>
+		<button class="btn variant-filled" on:click={login}>Login</button>
+	</div>
+</div>
